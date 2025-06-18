edf13fbc62d78673ff52a8a6c6540efa9281cf25
Foridul Islam <foridulislamdise880@gmail.com>
2025-06-18 19:25:44 +0600

    Array Practice in js

---
+++ Array.js
@@ -0,0 +1,47 @@
+
+function linearSearch(arr,target) {
+    let data=0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]<target && arr[i]>30) {
+            console.log(arr[i])
+        }
+        
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]===target) {
+            console.log(`your found data is ${arr[i]} index ${i}`)
+        }
+        if (arr[i]<target) {
+            data +=arr[i];
+        }
+    }
+    console.log(data)
+    console.log(data/arr.length)
+
+}
+
+let arr=[10,30,3,4,55,22,34,33,43,23,53,23,223];
+let target=53;
+
+linearSearch(arr,target);
+
+
+
+function minMaxValue (arr,target){
+   let min=arr[0];
+   let max=arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]<min) {
+            min=arr[i]
+        }
+        if (arr[i]>max) {
+            max=arr[i];
+        }
+        
+    }
+    console.log(`minimum number is ${min}`)
+    console.log(`maximum number is ${max}`)
+
+}
+minMaxValue(arr,target);
