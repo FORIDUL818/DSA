c92a27f41b58a36e7ccfaea0989140d2e854ee40
Foridul Islam <foridulislamdise880@gmail.com>
2025-06-29 22:06:02 +0600

    error solve

--- Array.js
+++ Array.js
@@ -29,23 +29,7 @@ linearSearch(arr2,target);
 
 
 
-function minMaxValue (arr,){
-   let min=arr[0];
-   let max=arr[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]<min) {
-            min=arr[i]
-        }
-        if (arr[i]>max) {
-            max=arr[i];
-        }
-        
-    }
-    console.log(`minimum number is ${min}`)
-    console.log(`maximum number is ${max}`)
 
-}
-minMaxValue(arr,target);
 
 
 
@@ -198,3 +182,41 @@ for (let  i = 0;  i < numbers.length;  i++) {
 }
 
 console.log( "dfdllfd", numbers)
+
+
+// merge sort start 
+  function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  // বাকি উপাদান যোগ করো
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+
+console.log(mergeSort(arr));
+
+// merge sort end
